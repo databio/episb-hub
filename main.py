e834cfecdccfbc5cc1d3b4bd43ffdaafc3607467
Ognen Duzlevski <oddodaoddo@gmail.com>
2019-01-14 09:42:29 -0500

    #14 - Fixes to the merge in production

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ app = Flask(__name__)
 
 # uncomment the following five lines if running on localhost
 # with episb-provider running in default setup
-#'es_host':'127.0.0.1',
-#'es_path':'',
-#'es_port':'8080',
-#'flask_host':'127.0.0.1',
-#'flask_port':'5000'
+#es_host=127.0.0.1',
+#es_path='',
+#es_port='8080',
+#flask_host='127.0.0.1',
+#flask_port='5000'
 
 # production mode settings on "live" episb.org server
-'es_host':'10.250.124.183',
-'es_path':'/episb-provider',
-'es_port':'8080',
-'flask_host':'episb.org',
-'flask_port':''
+es_host='10.250.124.183',
+es_path='/episb-provider',
+es_port='8080',
+flask_host='episb.org',
+flask_port=''
 
 @app.route('/region/<chrom>/<start>/<stop>')
 def render_segments(chrom,start,stop):
   check_start_stop(start,stop)
   url = "http://" + es_host + ":" + es_port + es_path + "/get/fromSegment/" + chrom + "/" + start + "/" + stop
-  print(url)
   try:
     url_req = urllib2.urlopen(url)
     query_json = json.load(url_req)
@@ -41,7 +40,7 @@ def render_segments_json(chrom,start,stop):
 
 @app.route('/api/v1/segmentations')
 def render_segmentations():
-  url = "http://" + es_host + es_port + es_path + "/segmentations/get/all"
+  url = "http://" + es_host + ":" + es_port + es_path + "/segmentations/get/all"
   try:
     url_req = urllib2.urlopen(url)
     query_json = json.load(url_req)
@@ -67,7 +66,10 @@ def get_segments():
   start = request.form.get("start")
   stop = request.form.get("stop")
   check_start_stop(start,stop)
-  url = "http://" + flask_host + ":" + flask_port + "/region/" + chrom + "/" + start + "/" + stop
+  if flask_port == '':
+    url = "http://" + flask_host + "/region/" + chrom + "/" + start + "/" + stop
+  else:
+    url = "http://" + flask_host + ":" + flask_port + "/region/" + chrom + "/" + start + "/" + stop
   return redirect(url, code=302)
 
 @app.route('/')
@@ -89,11 +91,4 @@ def check_start_stop(start,stop):
     return render_template("error.html", errmsg="STOP value must be greater than or equal to START value")
 
 if __name__ == '__main__':
-  from argparse import ArgumentParser
-  parser = ArgumentParser()
-  parser.add_argument('-m')
-  args = parser.parse_args()
-
-  run_env = args.m
-
-  app.run(host='0.0.0.0',port=8888,runenv=run_env,debug=True)
+  app.run(host='0.0.0.0',port=8888,debug=True)
